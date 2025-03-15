306cfe94c57422790e7144868bc8f395390c4c0a
DT (Mac) <devmastery1@gmail.com>
2025-03-15 09:37:17 +0300

    change form errors message in client reactjs

--- client-reactjs/src/features/form/form-errors.ts
+++ client-reactjs/src/features/form/form-errors.ts
@@ -3,17 +3,24 @@ import { FieldErrors } from 'react-hook-form';
 export default function formErrors(errors: FieldErrors): string[] {
     const parsedErrors: string[] = [];
     Object.keys(errors).map((key) => {
+
+        const errorMessage = errors[key]?.message as string
+
         if (errors[key]?.type === 'required') {
-            parsedErrors.push(key + ' is required');
+            parsedErrors.push(
+                (errorMessage || key + ' is required')
+            );
         }
         if (errors[key]?.type === 'minLength') {
             parsedErrors.push(
-                key + (errors[key]?.message ?? ' minLength is errored'),
+                (errorMessage || key + ' minLength is errored'),
             );
         }
 
         if (errors[key]?.type === 'pattern') {
-            parsedErrors.push(key + ' is invalid');
+            parsedErrors.push(
+                (errorMessage || key + ' is invalid')
+            );
         }
     });
     return parsedErrors;
